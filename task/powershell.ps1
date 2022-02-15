b24dafed1024bde3d8c16893755b7b6f0d5de36b
Sam Cogan <mail@samcogan.com>
2022-02-15 20:59:38 +0000

    Feature/recurse flag (#59)
    * add recurse flag

--- task/powershell.ps1
+++ task/powershell.ps1
@@ -13,6 +13,7 @@ $mainTemplateString = get-VstsInput -Name mainTemplates
 [boolean]$allTemplatesAreMain = get-VstsInput -Name allTemplatesMain -AsBool
 [boolean]$cliOutputResults = get-VstsInput -Name cliOutputResults -AsBool
 [boolean]$ignoreExitCode = get-VstsInput -Name ignoreExitCode -AsBool
+[boolean]$recurse = get-VstsInput -Name recurse -AsBool
 
 if($TestString){
     $Test=$TestString.split(',').trim()
@@ -36,4 +37,4 @@ else{
 }
 
 
-Invoke-TTK -templatelocation $templatelocation  -resultlocation $resultlocation -Test $Test -Skip $Skip -mainTemplates $mainTemplates -allTemplatesAreMain $allTemplatesAreMain -cliOutputResults $cliOutputResults -ignoreExitCode $ignoreExitCode
+Invoke-TTK -templatelocation $templatelocation  -resultlocation $resultlocation -Test $Test -Skip $Skip -mainTemplates $mainTemplates -allTemplatesAreMain $allTemplatesAreMain -cliOutputResults $cliOutputResults -ignoreExitCode $ignoreExitCode -recurse $recurse
